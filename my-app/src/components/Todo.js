cabc267a8c01d2ec270671c32649923d7ca1c9b4
Ankit Kumar Dubey <35380133+ankitdubey987@users.noreply.github.com>
2021-02-24 21:40:13 +0530

    now working with changing states when some event occurs

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -3,8 +3,23 @@ import { React } from "react";
 export default function Todo(params) {
     return(
         <div className='todo-items'>
-            <input type='checkbox' />
-            <p>{params.item.name}</p>
+            <input 
+                type='checkbox' 
+                onChange={()=>{console.warn('checkbox clicked!')}} 
+                checked={params.item.completed}
+            />
+            <p onMouseOver={handleMouseOver}>{params.item.name}</p>
+            <button onClick={handleClick}>
+                Click Me!
+            </button>
         </div>
     )
+}
+
+function handleClick() {
+    console.log('I was clicked !')
+}
+
+function handleMouseOver() {
+    console.log('Mouse was over me!')
 }
