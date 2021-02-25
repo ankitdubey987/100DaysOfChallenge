02a588b75b91052cee44f0f3144dded7f05203bb
Ankit Kumar Dubey <35380133+ankitdubey987@users.noreply.github.com>
2021-02-26 00:14:59 +0530

    added jokes with button to get random jokes

--- my-app/src/components/Fetcher.js
+++ my-app/src/components/Fetcher.js
@@ -6,32 +6,45 @@ export default class Fetcher extends Component{
         super()
         this.state = {
             loading:false,
-            character:[]
+            character:null
         }
+        this.fetchJoke = this.fetchJoke.bind(this)
     }
 
     componentDidMount(){
         this.setState({loading:true})
-        
-        setInterval(
-            fetch("https://api.chucknorris.io/jokes/random")
-            .then(res=>res.json())
-            .then(data=>{
-                this.setState((prevState=>{
-                    return{
-                        loading: false,
-                        character: data.value
-                    }
-                }))
-            }),150
-        )
+        fetch("https://api.chucknorris.io/jokes/random")
+        .then(res=>res.json())
+        .then(data=>{
+            this.setState({
+                loading: false,
+                character: data.value
+            })
+        })
     }
+
+    fetchJoke(){
+        this.setState({loading:true})
+        fetch("https://api.chucknorris.io/jokes/random")
+        .then(res=>res.json())
+        .then(data=>{
+            this.setState({
+                loading: false,
+                character: data.value
+            })
+        })
+    }
+    
 
     render(){
         const text = this.state.loading ? "loading...":this.state.character
         return(
             <div className="fetcher">
-                Jokes are {text}
+                <p>Jokes are {text}</p>
+                
+                <button onClick={this.fetchJoke}>
+                    Fetch Joke
+                </button>
             </div>
         )
     }
