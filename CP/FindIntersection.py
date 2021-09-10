841e9d1106296a057849586fd1df71b54594e450
ankitkumardubey-dev <ankitdubey.in@wordbite.com>
2021-09-10 22:36:36 +0530

    FindIntersection

---
+++ CP/FindIntersection.py
@@ -0,0 +1,11 @@
+# FindIntersection.py
+def FindIntersection(strArr:list)->str:
+  setOne = set(strArr[0].split(", "))
+  setTwo = set(strArr[1].split(", "))
+
+  result = sorted(list(setOne.intersection(setTwo)), key=lambda str: int(str))
+  
+  return ','.join(result) if len(result) > 0 else False
+
+# keep this function call here 
+print(FindIntersection(input()))
